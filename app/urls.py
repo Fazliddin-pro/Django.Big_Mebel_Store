a4c84d9a8faa153cfdbaea1f3fe36b68eb2b63c5
Fazliddin-Pro <sarofiddinovfazliddin72@gmail.com>
2024-12-02 14:54:10 +0500

    Login form and controller are ready

--- app/urls.py
+++ app/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    from debug_toolbar.toolbar import debug_toolbar_urls
-    urlpatterns += debug_toolbar_urls()
+    from debug_toolbar import urls as debug_toolbar_urls
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar_urls)),
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
