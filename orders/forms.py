36f22ae3a16b4d86ea047961c9d95656c3aca397
Fazliddin-Pro <sarofiddinovfazliddin72@gmail.com>
2024-12-07 12:04:24 +0500

    Create order is ready

--- orders/forms.py
+++ orders/forms.py
@@ -1,14 +1,27 @@
 from django import forms
 
 
-class CreateOrderForm(forms.From):
+class CreateOrderForm(forms.Form):
 
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
-    requires_delivery = forms.ChoiceField()
+    requires_delivery = forms.ChoiceField(
+        choices=[
+            ("0", False),
+            ("1", True),
+            ],
+        )
     delivery_address = forms.CharField(required=False)
-    payment_on_get = forms.ChoiceField()
+    payment_on_get = forms.ChoiceField(
+        choices=[
+            ("0", 'False'),
+            ("1", 'True'),
+            ],
+        )
+
+
+
 
 
     # first_name = forms.CharField(
@@ -42,27 +55,28 @@ class CreateOrderForm(forms.From):
     #     widget=forms.RadioSelect(),
     #     choices=[
     #         ("0", False),
-    #         ("1", True)
+    #         ("1", True),
     #     ],
-    #     initial=0
+    #     initial=0,
     # )
 
     # delivery_address = forms.CharField(
     #     widget=forms.Textarea(
     #         attrs={
     #             "class": "form-control",
-    #             "id": "delivery_address",
+    #             "id": "delivery-address",
     #             "rows": 2,
-    #             "placeholder": "Номер телефона",
+    #             "placeholder": "Введите адрес доставки",
     #         }
-    #     )
+    #     ),
+    #     required=False,
     # )
 
     # payment_on_get = forms.ChoiceField(
     #     widget=forms.RadioSelect(),
     #     choices=[
-    #         ("0", False),
-    #         ("1", True)
+    #         ("0", 'False'),
+    #         ("1", 'True'),
     #     ],
-    #     initial="card"
+    #     initial="card",
     # )
